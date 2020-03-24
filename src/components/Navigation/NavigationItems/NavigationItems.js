d1ec5c428d332c7eec6aa97702db09328e2cbe7e
Patrick Minda <patrickminda@hotmail.com>
2020-03-24 15:24:36 +1100

    commiting all files for final burger builder project

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+const navigationItems = (props) => (
+
+  <ul className={classes.NavigationItems}>
+      <NavigationItem link="/" exact> Burger Builder </NavigationItem>
+      {props.isAuthenicated ? <NavigationItem link="/orders"> Orders </NavigationItem> : null}
+      {!props.isAuthenicated 
+            ? <NavigationItem link="/auth"> Authenicate </NavigationItem>
+          : <NavigationItem link="/logout"> Logout </NavigationItem>
+      }
+  </ul>
+
+);
+
+export default navigationItems;
